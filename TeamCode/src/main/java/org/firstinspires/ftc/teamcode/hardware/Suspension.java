5c39179023f9e41bbcce7e8ee5ba2c0e878dbcb4
Will Rages <rageswill@pleasval.org>
2023-12-19 17:31:23 -0600

    Screw you jaren

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Suspension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Suspension.java
@@ -6,20 +6,20 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.utility.Api;
 
 public class Suspension {
-    private final DcMotor left;
-    private final DcMotor right;
+    private final DcMotor leftScrew;
+    private final DcMotor rightScrew;
 
     public Suspension(HardwareMap hardwareMap) {
-        left = hardwareMap.get(DcMotor.class, "Left Arm");
-        right = hardwareMap.get(DcMotor.class, "Right Arm");
+        leftScrew = hardwareMap.get(DcMotor.class, "Left Screw");
+        rightScrew = hardwareMap.get(DcMotor.class, "Right Screw");
     }
 
     /**
      * Sets both arm powers.
      * @param power The power to set. Positive is up. [-1, 1].
      */
-    public @Api void setArmPower(double power) {
-        left.setPower(power);
-        right.setPower(power);
+    public @Api void screwDaScrew(double power) {
+        leftScrew.setPower(power);
+        rightScrew.setPower(power);
     }
 }
