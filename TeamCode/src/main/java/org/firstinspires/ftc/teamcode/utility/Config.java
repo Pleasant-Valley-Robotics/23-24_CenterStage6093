73a05090109a5e2850ca7c0d0dfec7943bd2aefe
BritneyBick <148918388+BritneyBick@users.noreply.github.com>
2023-12-19 17:20:18 -0600

    Changed drive coefficiants for Calypso.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/Config.java
@@ -4,9 +4,10 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 public class Config {
     // Encoder/Inch = 360/Ticks per motor shaft revolution
-    public static final double TICKS_PER_REVOLUTION = 537.6; //Encoder pulses per rev by supplier information
-    public static final double GEARBOX_REDUCTION = 7.0 / 9.0; //Gearbox on motor * gearbox on robot
-    public static final double WHEEL_DIAMETER_INCHES = 4; //Wheel diameter in inches
+    public static final double TICKS_PER_REVOLUTION = 3007.2; //Encoder pulses per rev by supplier information
+    public static final double GEARBOX_REDUCTION = 1; //Gearbox on motor * gearbox on robot 26.9:1, this is already included in
+    //ticks per revoloution, given by goBuildA.
+    public static final double WHEEL_DIAMETER_INCHES = 5.512; //Wheel diameter in inches
     public static final double ENCODER_PER_INCH = (TICKS_PER_REVOLUTION * GEARBOX_REDUCTION) / (WHEEL_DIAMETER_INCHES * Math.PI);
 
     public static final RevHubOrientationOnRobot HUB_FACING = new RevHubOrientationOnRobot(
