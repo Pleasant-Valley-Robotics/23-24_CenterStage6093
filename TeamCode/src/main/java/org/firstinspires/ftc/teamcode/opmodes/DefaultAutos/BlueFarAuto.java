f3f7f48b36d9bb54bfbc1b2a617c72e0cecc7fc4
FTC-6093 <89921520+Mahoroah@users.noreply.github.com>
2023-12-22 10:18:07 -0600

    More accurate left and middle spikemarks placing and scoring.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DefaultAutos/BlueFarAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DefaultAutos/BlueFarAuto.java
@@ -32,31 +32,65 @@ public class BlueFarAuto extends LinearOpMode {
         telemetry.update();
 
         drivebase.driveForward(24, 0.3, telemetry);
+        camera.disableDetection();
 
         if(side == CubeSide.Left) {
             drivebase.relativeTurn(65, 0.4, telemetry);
-            drivebase.driveForward(6.0, 0.3, telemetry);
-            drivebase.driveForward(-6.0, 0.3, telemetry);
+            drivebase.driveForward(7.0, 0.3, telemetry);
+            drivebase.driveForward(-7.0, 0.3, telemetry);
             drivebase.absoluteTurn(0, 0.4, telemetry);
         }
 
         switch (side) {
             case Right:
-                //was 65 before
-
+                drivebase.driveForward(28, 0.4, telemetry);
+                drivebase.absoluteTurn(150, 0.2, telemetry);
+                drivebase.driveForward(8,0.4, telemetry);
+                drivebase.driveForward(-10, 0.4, telemetry);
+                drivebase.relativeTurn(30, 0.4, telemetry);
                 break;
             case Left:
-                drivebase.driveForward(13, 0.3, telemetry);
+                //Was 23 before.
+                drivebase.driveForward(26, 0.3, telemetry);
                 drivebase.absoluteTurn(180, 0.3, telemetry);
                 break;
             case Middle:
-                drivebase.driveForward(29, 0.4, telemetry);
+                drivebase.driveForward(20, 0.25, telemetry);
+                drivebase.driveForward(4, 0.3, telemetry);
                 drivebase.absoluteTurn(180, 0.2, telemetry);
-                drivebase.driveForward(5,0.4, telemetry);
-                drivebase.driveForward(-5, 0.4, telemetry);
+                drivebase.driveForward(4,0.4, telemetry);
+                drivebase.driveForward(-8, 0.4, telemetry);
                 break;
         }
         // Score Pixel
         telemetry.update();
+
+        drivebase.absoluteTurn(90, 0.4, telemetry);
+        drivebase.driveForward(80, 0.5, telemetry);
+        drivebase.driveSideways(-36, 0.4, telemetry);
+
+        switch(side) {
+            case Left:
+                //Offset
+                drivebase.driveSideways(-8, 0.3, telemetry);
+                //Center To Apriltag
+                break;
+            case Right:
+                //Offset
+                drivebase.driveSideways(6, 0.3, telemetry);
+                //Center to Apriltag
+                break;
+            case Middle:
+                //Was -6 before.
+                drivebase.driveSideways(-5, 0.3, telemetry);
+
+                //Center to Apriltag
+                break;
+        }
+
+        drivebase.driveForward(7, 0.3, telemetry);
+
+        dropper.dropPixel();
+        drivebase.driveForward(-2, 0.5, telemetry);githu
     }
 }
